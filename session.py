5750abcc3f0b36b3a5886fdc56647ec1e772a1a3
Ashqalsmt <128143959+Ashqalsmt@users.noreply.github.com>
2025-05-29 10:56:26 +0300

    Add files via upload

---
+++ session.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
+    client.sendmessage("me", client.session.save())
